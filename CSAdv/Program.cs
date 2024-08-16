5ced3fe1b62e6fd3eb9c387dd7c4cba6f357bb8f
Yehang-Kim <saasaa2y@gmail.com>
2024-08-16 11:19:24 +0900

    인덱서 실습

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -21,6 +21,15 @@ namespace CSAdv
     {
 
     }
+
+    class SquareCalculator
+    {
+        public int this[int i]
+        {
+            get { return i * 2; }
+            set { Console.WriteLine("{i}번째 상품 설정", i); }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -33,7 +42,11 @@ namespace CSAdv
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedDouble.Value);
 
-
+            SquareCalculator sc = new SquareCalculator();
+            Console.WriteLine(sc[10]);
+            Console.WriteLine(sc[11]);
+            Console.WriteLine(sc[40]);
+            sc[3] = 4;
         }
     }
 }
